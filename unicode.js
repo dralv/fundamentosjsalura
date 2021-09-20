097c51cb97f35d7d960c1c7903ea1d11c99a2d72
Alvaro <dralvphd2@gmail.com>
2021-09-20 10:50:38 -0300

    Adicionando arquivos

---
+++ unicode.js
@@ -0,0 +1,16 @@
+// const cifrao = '\u0024'
+// const aMaiusculo = '\u0041'
+// const tique = '\u2705'
+// const hiragana = '\u3041'
+
+// console.log(cifrao)
+// console.log(aMaiusculo)
+// console.log(tique)
+// console.log(hiragana)
+
+const cidade = "TERESINA";
+const input ="teresina";
+
+const cidadeModificada = cidade.toLowerCase();
+
+console.log(cidadeModificada === input);
