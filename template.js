5358d74eb15ce47b2e22ba6cb6d27b8d4567d507
Alvaro <dralvphd2@gmail.com>
2021-09-22 08:27:56 -0300

    Adicionando novos arquivos

---
+++ template.js
@@ -0,0 +1,11 @@
+const nome = "Álvaro"
+
+const idadeMinima = 18;
+const minhaIdade = 17;
+
+
+
+
+const apresentação = `Meu nome é ${nome} eu tenho ${minhaIdade} anos e eu irei beber ${minhaIdade>=idadeMinima ? "cerveja":"suco"}`
+
+console.log(apresentação)
--- variaveis.js
+++ variaveis.js
@@ -4,3 +4,5 @@ let numeroString = "4";
 console.log(numero===numeroString);
 console.log(numero==numeroString);
 console.log(numero==Number(numeroString));
+
+console.error("erro evidente");
